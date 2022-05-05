873985cbb7e7b2c812687c560c4de55b7818dd4f
Eli <eli.r.stanley@gmail.com>
2022-05-04 22:57:24 -0400

    rinkeby test 5

--- backend/src/config.js
+++ backend/src/config.js
@@ -99,7 +99,7 @@ const TOKENS_PER_MINT = 5; // maximum number of NFTs a user can mint in a single
 const PUBLIC_MINT_START_DATE = "2022-06-04T05:00:00+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2022-05-04T05:00:00+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-06-01T05:00:00+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x052Cf3CD27dCc49105924fB5d691Cac75EAAC7DC"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
